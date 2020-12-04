0bdc981dac47b5470bb0394ff074867fa50c62b7
Nathan Ladd <nathanladd@gmail.com>
2020-12-04 14:39:19 -0600

    Entity ID can be specified to handler fixture

---
+++ test/automated/handler_fixture/setup/entity/specified_with_entity_id.rb
@@ -0,0 +1,44 @@
+require_relative '../../../automated_init'
+
+context "Handler Fixture" do
+  context "Setup" do
+    context "Stored Entity" do
+      context "Specified" do
+        context "Entity ID is Specified" do
+          input_message = Controls::Message.example
+
+          entity = Controls::Entity::Identified.example
+          entity_id = Identifier::UUID::Random.get
+
+          handler = Controls::Handler.example
+
+          fixture = Handler.build(
+            handler,
+            input_message,
+            entity,
+            11,
+            entity_id
+          )
+
+          store = handler.store
+
+          stored_entity, version = store.get(entity_id, include: :version)
+
+          refute(stored_entity.nil?)
+
+          test "Fixture entity is stored in handler's store" do
+            assert(stored_entity == entity)
+          end
+
+          context "Cache Record" do
+            cache_record = store.records[entity_id]
+
+            test "Entity ID is the cache record's ID" do
+              assert(cache_record.id == entity_id)
+            end
+          end
+        end
+      end
+    end
+  end
+end
